c1d2da0ae852123049a6c3e432713f8e05e45391
Hkumar145 <67310940+Hkumar145@users.noreply.github.com>
2025-06-23 02:54:34 -0700

    favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,6 +12,7 @@ export default function Header() {
 
       <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
         <h1 className="text-2xl font-bold text-gray-800">Hemant Kumar</h1>
+        
 
         {/* Hamburger Button for Mobile */}
         <button
